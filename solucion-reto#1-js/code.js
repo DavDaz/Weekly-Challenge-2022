7f5a05cc577c55761e034e0452c655f022a5c372
DavDaz <ddiazlink08@gmail.com>
2022-08-31 20:17:02 -0500

    Solución Reto #1 JS

--- solucion-reto#1-js/code.js
+++ solucion-reto#1-js/code.js
@@ -2,34 +2,42 @@ var container = document.getElementById("container");
 var palabra1 = document.getElementById("palabra1");
 var palabra2 = document.getElementById("palabra2");
 var boton = document.getElementById("boton");
-var resultado = Boolean;
 
 boton.addEventListener('click', isAnagrama);
-/*
-********** Mi solucion que fue una mielda ********
-
-boton.addEventListener('click', revisarAnagrama);
-
-function revisarAnagrama(){
-    arrPalabra1 = Array.from(palabra1.value);
-    arrPalabra2 = Array.from(palabra2.value);
-
-    if(arrPalabra1.length === arrPalabra2.length){
-        for(i = 0; i <= arrPalabra1.length -1; i++){
-            if( arrPalabra1.includes(arrPalabra2[i]) ){
-                resultado = true;
-            }else{
-                resultado = false;
-            }
-        }
-    }else{
-        console.log("Son diferentes");
-    }
-    container.innerHTML += "La validacion Anagrama es: "+ resultado + "<br>"
-}
-*/
 
 function isAnagrama(){
-    if(palabra1.value === palabra2.value)
-    return false
+    word1 = palabra1.value;
+    word2 = palabra2.value;
+    var resultado = word1.toLowerCase().split("").sort().join() === word2.toLowerCase().split("").sort().join();
+    container.innerHTML += "¿Son Anagramas ["+ word1 +"] = ["+ word2 +"]? : "+ resultado +"<br>"
+  
 }
+
+
+/*
+function esAnagrama(palabra, posibleAnagrama){
+  // Cambiar a minúsculas ambas cadenas
+  palabra = palabra.toLowerCase();
+  posibleAnagrama = posibleAnagrama.toLowerCase();
+  
+  // Convertir ambas cadenas en un arreglo
+  palabra = palabra.split("");
+  posibleAnagrama = posibleAnagrama.split("");
+  
+  // Ordenar ese arreglo
+  palabra = palabra.sort();
+  posibleAnagrama = posibleAnagrama.sort();
+  
+  // Una vez ordenados, los convertimos a cadena nuevamente
+  palabra = palabra.join("");
+  posibleAnagrama = posibleAnagrama.join("");
+  
+  // Finalmente comparamos
+  
+  if(palabra === posibleAnagrama){
+    return true;
+  }else{
+    return false;
+  }
+}
+*/
